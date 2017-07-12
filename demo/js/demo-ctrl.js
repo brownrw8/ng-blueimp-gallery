48ff1ee24d36c6bd61ca686fbb4a96f7f746975d
rbrown <Robert-Brown@idexx.com>
2017-07-12 16:15:33 -0400

    update

--- demo/js/demo-ctrl.js
+++ demo/js/demo-ctrl.js
@@ -11,65 +11,53 @@
 
         $scope.list = [
             {
-              href: 'images/1.png',
-              thumb: 'images/1.png',
+              href: 'images/image1.jpg',
+              thumb: 'images/image1_thumb.jpg',
               title: '1.png',
               selected: false
             },
             {
-              href: 'images/2.png',
-              thumb: 'images/2.png',
+              href: 'images/image2.jpg',
+              thumb: 'images/image2_thumb.jpg',
               title: '2.png',
               selected: false
             },
             {
-              href: 'images/3.png',
-              thumb: 'images/3.png',
+              href: 'images/image3.jpg',
+              thumb: 'images/image3_thumb.jpg',
               title: '3.png',
               selected: false
             },
             {
-              href: 'images/4.png',
-              thumb: 'images/4.png',
+              href: 'images/image4.jpg',
+              thumb: 'images/image4_thumb.jpg',
               title: '3.png',
               selected: false
             },
             {
-              href: 'images/5.png',
-              thumb: 'images/5.png',
-              title: '5.png',
+              href: 'images/image1.jpg',
+              thumb: 'images/image1_thumb.jpg',
+              title: '1.png',
               selected: false
             },
             {
-              href: 'images/6.png',
-              thumb: 'images/6.png',
-              title: '6.png',
+              href: 'images/image2.jpg',
+              thumb: 'images/image2_thumb.jpg',
+              title: '2.png',
               selected: false
             },
             {
-              href: 'images/7.png',
-              thumb: 'images/7.png',
-              title: '7.png',
+              href: 'images/image3.jpg',
+              thumb: 'images/image3_thumb.jpg',
+              title: '3.png',
               selected: false
             },
             {
-              href: 'images/8.png',
-              thumb: 'images/8.png',
-              title: '8.png',
+              href: 'images/image4.jpg',
+              thumb: 'images/image4_thumb.jpg',
+              title: '3.png',
               selected: false
-            },
-            {
-              href: 'images/9.png',
-              thumb: 'images/9.png',
-              title: '9.png',
-              selected: false
-            },
-            {
-              href: 'images/10.png',
-              thumb: 'images/10.png',
-              title: '10.png',
-              selected: false
-            },
+            }
         ];
         
         $scope.download = function(items){
